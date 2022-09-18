da3227920d82afaf3ae3d12137e01a04eab4d9b2
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-18 22:01:25 +0600

    recursive fibonacci

---
+++ algorithms/14-recursive-fibonacci.js
@@ -0,0 +1,18 @@
+// Given a number 'n', find the nth element of the fibonacci sequence
+
+// [0,1,1,2,3,5,8,...n]
+// first two elements are 0 and 1
+// 6th element is 8
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(3));
+console.log(recursiveFibonacci(6));
+console.log(recursiveFibonacci(7));
