bf2939430653e743f93e8c168b450b71879e40bc
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-18 21:22:04 +0600

    factorial

---
+++ algorithms/10-factorial.js
@@ -0,0 +1,19 @@
+// Given an integer 'n', find the factorial of that integer
+
+// 0! = 1
+// 4! = 4 * 3 * 2 * 1
+// 5! = 5* 4 * 3 * 2 * 1
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(10)); // O(n) - linear
