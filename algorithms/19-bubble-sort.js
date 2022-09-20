dcea0bc531130d4fc19a1bba846a5c012c36d663
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-21 02:48:43 +0600

    bubble sort added

---
+++ algorithms/19-bubble-sort.js
@@ -0,0 +1,21 @@
+// Given an array of integers, sort the array.
+
+function bubbleSort(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const arr = [8, 20, -4, -2, -6];
+bubbleSort(arr);
+console.log(arr); // O(n^2) - quadratic
